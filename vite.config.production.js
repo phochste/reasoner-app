e0e65b37f706c8749308cb01b03acaf7ab696472
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-05-28 14:36:46 +0200

    Adding latest eye reasoner

---
+++ vite.config.production.js
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: 'https://w3c-cg.github.io/rdfsurfaces/demonstrator/', 
+  css: {
+    devSourcemap: true,
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  // https://github.com/jonathanpmartins/v-money3/issues/70#issuecomment-1284503693
+  optimizeDeps: {
+    esbuildOptions: {
+      target: ["es2020", "safari14"],
+    },
+  },
+  build: {
+    target: ["es2020", "safari14"],
+  },
+});
